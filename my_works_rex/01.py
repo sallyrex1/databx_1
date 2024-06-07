4eca14c27f43412b6182d9b2027857dd2de30b16
sallyrex1 <sallyrex1>
2024-06-07 12:23:51 +0000

    1stCopy
    This is the fist copy

---
+++ my_works_rex/01.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+print('just testing')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC SELECT 'I am running a Magic command' AS tb1
+
+# COMMAND ----------
+
+# use the full path not the relative or url
+%run "/Workspace/Users/rexford.osei-aboagye@cognizant.com/databx_1/my_works_rex/Utilty_NoteBook"
+
+# COMMAND ----------
+
+
---
+++ my_works_rex/Utilty_NoteBook.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC SELECT 'This is coming from the Utility Notebook' AS UtilityNotes
+
+# COMMAND ----------
+
+
